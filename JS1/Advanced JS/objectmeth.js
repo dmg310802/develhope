1d07ad76adebf8e39ed11ecf434a6aae14be0d47
dmg310802 <danielmtz@next2.com.mx>
2023-09-22 19:17:27 +0200

    Exercise 9: Object Methods

---
+++ JS1/Advanced JS/objectmeth.js
@@ -0,0 +1,15 @@
+// Create an object person. Print its key/value pair in the console. Try to use the method Object.keys:
+
+// firstName: Mario
+// lastName: Rossi
+// age: 25
+
+
+
+let person = {
+firstName: "Mario",
+lastName: "Rossi",
+ age: 25
+};
+
+console.log(Object.entries(person));
