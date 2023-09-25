32e0bb418fe581de5c5b3d8cead17b08bfd92ede
dmg310802 <danielmtz@next2.com.mx>
2023-09-25 14:46:53 +0200

    Exercise 18

--- JS1/Advanced JS/static.js
+++ JS1/Advanced JS/static.js
@@ -1,7 +1,18 @@
+// Define a static method called fromObject inside the Person class 
+// which takes in an object literal as parameter and instantiate a Person object.
+
 class Person {
+    firstName = "";
+    lastName = "";
+    static obj;
+    
     constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
+    }
+    static fromObject(obj = "") {
+        let person = new Person(obj.firstName,obj.lastName);
+        return person; 
     }
     }
     
