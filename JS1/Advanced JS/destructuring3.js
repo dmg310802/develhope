ea4453b03c7a28490b9d450439a75873613ae94e
dmg310802 <danielmtz@next2.com.mx>
2023-10-11 19:53:22 +0200

    exercise 23

---
+++ JS1/Advanced JS/destructuring3.js
@@ -0,0 +1,19 @@
+// Use the destructuring to make the check of the age easier. Try modifying the parameter of the isAdult function.
+
+function isAdult(person) {
+  if (person.age >= 18) {
+    return "Is an adult";
+  } else {
+    return "Not an adult";
+  }
+  
+}
+
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 13
+};giot
+
+console.log(isAdult(person));
