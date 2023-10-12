0b93a6de8dbb6dccb857c1c7d1f8923b812dc090
dmg310802 <danielmtz@next2.com.mx>
2023-10-12 13:35:46 +0200

    exercise 26: rest2

--- JS1/Advanced JS/rest2.js
+++ JS1/Advanced JS/rest2.js
@@ -1,8 +1,12 @@
 // The sum function has a high numbers of parameters. 
 // How can we improve the code in order to make it accept any amount of numbers to sum as its argument?
 
-function sum(num1, num2, num3, num4, num5) {
-    return num1 + num2 + num3 + num4 + num5;
+function sum(...nums) {
+   let totalSum = 0;
+   for (const num of nums) {
+        totalSum += num;
+   }
+   return totalSum;
 }
 
-console.log(sum(1, 2, 3, 4, 5));
+console.log(sum(1, 2, 3, 4,5,6));
