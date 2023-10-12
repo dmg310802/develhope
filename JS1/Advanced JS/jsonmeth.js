274443fe1d1c7f4536bb2d03a6f169eee6eaace0
dmg310802 <danielmtz@next2.com.mx>
2023-10-12 19:14:04 +0200

    exercise 29: json1

--- JS1/Advanced JS/jsonmeth.js
+++ JS1/Advanced JS/jsonmeth.js
@@ -1,8 +1,17 @@
-// Perform a code refactoring by using the Spread Operator. The output of the console.log must be the same.
+// Convert the developer object into JSON
 
-function sum(num1, num2, num3) {
-  return num1 + num2 + num3;
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
 }
 
-const numbers = [1, 2, 3];
-console.log(sum(numbers[0], numbers[1], numbers[2]));
+const developer = new Person(1, 'Mario', 'Rossi', 25);
+
+let str = JSON.stringify(developer);
+
+console.log(str);
+// Print developer as JSON object
