8f8aeda8e42daf7525040626efaba9743a14997a
dmg310802 <104369426+dmg310802@users.noreply.github.com>
2023-10-18 05:56:19 -0500

    exercise 38: callbacks2

--- JS1/Advanced JS/callback2.js
+++ JS1/Advanced JS/callback2.js
@@ -1,3 +1,19 @@
 // Starting from the previous exercise, we want to add to our function repeatHello, a clearInterval after 5 seconds, could you do that?
 
 // The callback function must be an arrow function, can you also explain why?
+
+function repeatHello(callback) {
+    setInterval(()=> {
+        const sayingHello = `Hello`;
+        callback(sayingHello)
+    },1000
+    )
+}
+
+
+const print = (sayingHello) => [
+        console.log(sayingHello)
+   
+]
+
+repeatHello(print);
