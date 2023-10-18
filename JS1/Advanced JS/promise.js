4890b24e43c0eee7119e62062e235aa86f5500b5
dmg310802 <danielmtz@next2.com.mx>
2023-10-18 13:15:41 +0200

    exercise 38: promise

--- JS1/Advanced JS/promise.js
+++ JS1/Advanced JS/promise.js
@@ -1,3 +1,20 @@
-// Write a promise. If the variable called number is greater than 10, then it returns a resolve, otherwise it returns a reject. Then manage the promise with then and catch in case of any reject.
+// Write a promise. 
+// If the variable called number is greater than 10, 
+// then it returns a resolve, otherwise it returns a reject. 
+// Then manage the promise with then and catch in case of any reject.
 
 // const number = 15;
+
+
+const randomPromesa = new Promise((resolve,reject) => {
+    const number = 15;
+    if (number > 10) {
+        resolve('Promise resolved');
+    } else {
+        reject('Promise rejected');
+    }
+})
+
+randomPromesa
+    .then((val) => console.log(val))
+    .catch((err) => console.error(err))
