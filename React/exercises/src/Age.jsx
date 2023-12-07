e935a2c50b29a004699eddc5899ad4bd4f607be8
dmg310802 <danielmtz@next2.com.mx>
2023-12-07 01:19:43 +0100

    react exercise 9 CR 2

--- React/exercises/src/Age.jsx
+++ React/exercises/src/Age.jsx
@@ -2,6 +2,7 @@ function Age({age}) {
 
     return (
       <>
+       {age > 18 ? <p>You are {age} years old!</p> : <p>You are very young!</p>}
        {age > 18 && <p>You are {age} years old!</p>}
        {age && <p>You are {age} years old!</p>}
        {age > 18 && age <65 && <p>You are {age} years old!</p>}
